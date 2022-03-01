54bf1daf02ce6ce92ba5aba6af028275139eef98
christopher graner <chrisgraner1@gmail.com>
2022-03-01 09:54:34 -0800

    foundation

--- webapp.py
+++ webapp.py
@@ -8,17 +8,17 @@ def render_main():
 
 @app.route("/response")
 def render_response():
-    color = request.args['color'] 
+    deadlift = request.args['deadlift']
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if color == 'pink':
-        reply1 = "That's my favorite color, too!"
+    if deadlift >= '315':
+        reply1 = "Zyzz would be proud"
     else:
-        reply1 = "My favorite color is pink."
-    n = int(request.args['multNum']) #values in request.args are strings by default
-    reply2 = "2 x " + str(n) + " = " + str((2*n))
+        reply1 = "Lol beta weight"
+    n = int(request.args['deadlift']) #values in request.args are strings by default
+    reply2 = "I like cardio"
     return render_template('response.html', response1 = reply1, response2 = reply2)
-    
+
 if __name__=="__main__":
     app.run(debug=False)
