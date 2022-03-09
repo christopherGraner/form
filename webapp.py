c160ec33d26c3118b0e15bf8fa288939edf21489
christopher graner <chrisgraner1@gmail.com>
2022-03-09 09:51:53 -0800

    VGJ

--- webapp.py
+++ webapp.py
@@ -9,21 +9,36 @@ def render_main():
 @app.route("/response")
 def render_response():
     use = request.args['use']
+    brand = request.args['brand']
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
     reply1 =""
-    if use == "Racing":
-        reply1 = "Street racing is illigal. Don't do it."
-    elif use == "Ad":
-        reply1 = "You'll need somnething reliable you can thrash."
-    elif use == "FC":
-        reply1 = "You most likely want more than 4 seats and a large trunk."
+    if use == "Racing" and brand == "P":
+        reply1 = "You should get a Porsche GT3 RS."
+    elif use == "Ad" and brand == "P":
+        reply1 = "You should get a Posrche Cayanne Turbo S."
+    elif use == "FC" and brand == "P":
+        reply1 = "You should get a Porsche Macan GTS."
+    elif use == "Racing" and brand == "BMW":
+        reply1 = "You should get a BMW M4 CS."
+    elif use == "FC" and brand == "BMW":
+        reply1 = "You should get a BMW 5 series."
+    elif use == "Ad" and brand == "BMW":
+        reply1 = "You should get an BMW X8."
+    elif use == "Racing" and brand == "ford":
+        reply1 = "You should get a Ford GT."
+    elif use == "Ad" and brand == "ford":
+        reply1 = "You should get a Ford Explorer."
+    elif use == "FC" and brand == "ford":
+        reply1 = "You should get a Ford Fusion."
 
 
-    n = bool(request.args['use']) #values in request.args are strings by default
-    reply2 = ""
-    return render_template('response.html', response1 = reply1, response2 = reply2)
+ #values in request.args are strings by default
+
+
+
+    return render_template('response.html', response1 = reply1)
 
 if __name__=="__main__":
     app.run(debug=True)
